81b1b1aaeece5e317fc70489301adf277c0fd57f
aryanpatel05 <aryanpatel9279@gmail.com>
2023-08-28 16:27:00 +0530

    commit

--- aryan.js
+++ aryan.js
@@ -83,7 +83,7 @@ menu_items.forEach( (e) => {
             document.getElementById('driver-submenu').innerHTML = '';
         })
         
-    }else if( menu_child.text  === "HOME" ){
+    // }else if( menu_child.text  === "HOME" ){
     
         menu_child.addEventListener("mouseenter", (e) => {
             mega_menu.style.display='block'
@@ -123,3 +123,62 @@ function fetch_menu_content(menu_name){
       console.error('Error:', error);
     });
 }
+
+var textWrapper = document.querySelector('.no-margin .letters');
+textWrapper ? textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letters'>$&</span>") : '';
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.no-margin .letters',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: (el, i) => 140 * i
+  }).add({
+    targets: '.no-margin',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+let htmlCode = 'he';
+
+const file_text = async (filename) => {
+    try {
+        const response = await fetch(filename);
+        const html = await response.text();
+        return html;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+file_text('f1partners.html').then((result) => document.getElementById("default-f1-partners").innerHTML= result)
+
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.slider img');
+
+let slideIndex = 0;
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % images.length;
+    updateSliderPosition();
+}
+
+function updateSliderPosition() {
+    const offset = slideIndex * -100;
+    slider.style.transform = `translateX(${offset}%)`;
+}
+
+// Change slide every 3 seconds (adjust the interval as needed)
+const slideInterval = setInterval(nextSlide, 3000);
+
+// Pause auto-sliding when the user hovers over the slider
+slider.addEventListener('mouseenter', () => clearInterval(slideInterval));
+
+// Resume auto-sliding when the user moves the mouse out of the slider
+slider.addEventListener('mouseleave', () => {
+    slideInterval = setInterval(nextSlide, 3000);
+});
+
+  
