b06ec72e54c826b071c512152d78898e74c05a9a
aryanpatel05 <aryanpatel9279@gmail.com>
2023-10-01 17:03:44 +0530

    commit

--- aryan.js
+++ aryan.js
@@ -1,6 +1,6 @@
 var loader = document.getElementById("loader-1");
 
-window.addEventListener("load" , function(){
+window.addEventListener("load", function () {
     loader ? loader.style.display = "none" : '';
 })
 // ------------------------------------------------------------------------------------
@@ -71,8 +71,8 @@ function initClock() {
 
 
 
-  let menu = document.getElementById('driver-main-menu');
-  let mega_menu = document.getElementById('nav-secondary')
+let menu = document.getElementById('driver-main-menu');
+let mega_menu = document.getElementById('nav-secondary')
 
 
 //   menu.addEventListener("mouseenter", (e) => {
@@ -88,91 +88,91 @@ let menu_items = document.querySelectorAll('.item')
 
 
 
-menu_items.forEach( (e) => {
+menu_items.forEach((e) => {
     let menu_child = e.childNodes[0];
 
-    if( menu_child.text  === "DRIVER" ){
+    if (menu_child.text === "DRIVER") {
 
         menu_child.addEventListener("mouseenter", (e) => {
-            mega_menu.style.display='block'
+            mega_menu.style.display = 'block'
 
             fetch_menu_content('test.html')
         })
 
         menu.addEventListener("mouseleave", (e) => {
-            mega_menu.style.display='none'
+            mega_menu.style.display = 'none'
             document.getElementById('driver-submenu').innerHTML = '';
         })
 
         mega_menu.addEventListener("mouseenter", (e) => {
-            mega_menu.style.display='block'
+            mega_menu.style.display = 'block'
             fetch_menu_content('test.html')
 
         })
 
         mega_menu.addEventListener("mouseleave", (e) => {
-            mega_menu.style.display='none'
+            mega_menu.style.display = 'none'
             document.getElementById('driver-submenu').innerHTML = '';
         })
-        
-    // }else if( menu_child.text  === "HOME" ){
-    
+
+        // }else if( menu_child.text  === "HOME" ){
+
         menu_child.addEventListener("mouseenter", (e) => {
-            mega_menu.style.display='block'
+            mega_menu.style.display = 'block'
 
-             fetch_menu_content('test.html')
-          })
+            fetch_menu_content('test.html')
+        })
 
-          menu.addEventListener("mouseleave", (e) => {
-            mega_menu.style.display='none'
+        menu.addEventListener("mouseleave", (e) => {
+            mega_menu.style.display = 'none'
             document.getElementById('driver-submenu').innerHTML = '';
         })
 
         mega_menu.addEventListener("mouseenter", (e) => {
-            mega_menu.style.display='block'
+            mega_menu.style.display = 'block'
             fetch_menu_content('test.html')
 
         })
 
         mega_menu.addEventListener("mouseleave", (e) => {
-            mega_menu.style.display='none'
+            mega_menu.style.display = 'none'
             document.getElementById('driver-submenu').innerHTML = '';
         })
     }
 });
 
-function fetch_menu_content(menu_name){
-    document.getElementById('driver-submenu').innerHTML = '<div class="loader" style="background: none;height: 0px;"></div>' ;
+function fetch_menu_content(menu_name) {
+    document.getElementById('driver-submenu').innerHTML = '<div class="loader" style="background: none;height: 0px;"></div>';
     fetch(menu_name)
-    .then(response => response.text())
-    .then(html => {
-      // Store the HTML code in a JavaScript variable
-      const htmlCode = html;
-      
-      document.getElementById('driver-submenu').innerHTML = htmlCode;
-      // You can now use the htmlCode variable as needed
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+        .then(response => response.text())
+        .then(html => {
+            // Store the HTML code in a JavaScript variable
+            const htmlCode = html;
+
+            document.getElementById('driver-submenu').innerHTML = htmlCode;
+            // You can now use the htmlCode variable as needed
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
 var textWrapper = document.querySelector('.no-margin .letters');
 textWrapper ? textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letters'>$&</span>") : '';
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.no-margin .letters',
-    rotateY: [-90, 0],
-    duration: 1300,
-    delay: (el, i) => 140 * i
-  }).add({
-    targets: '.no-margin',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.no-margin .letters',
+//     rotateY: [-90, 0],
+//     duration: 1300,
+//     delay: (el, i) => 140 * i
+//   }).add({
+//     targets: '.no-margin',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
 
 let htmlCode = 'he';
 
@@ -186,7 +186,7 @@ const file_text = async (filename) => {
     }
 }
 
-file_text('f1partners.html').then((result) => document.getElementById("default-f1-partners") ? document.getElementById("default-f1-partners").innerHTML= result : '') 
+file_text('f1partners.html').then((result) => document.getElementById("default-f1-partners") ? document.getElementById("default-f1-partners").innerHTML = result : '')
 
 
 const slider = document.querySelector('.slider');
@@ -215,7 +215,7 @@ slider ? slider.addEventListener('mouseleave', () => {
     slideInterval = setInterval(nextSlide, 3000);
 }) : '';
 
-  
+
 
 // const slider = document.querySelector('.slider');
 // const images = document.querySelectorAll('.slider img');
@@ -244,34 +244,34 @@ slider ? slider.addEventListener('mouseleave', () => {
 //     slideInterval = setInterval(nextSlide, 3000);
 // });
 
-$( document ).ready(function() {
+$ (document).ready(function () {
     $('.slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        pauseOnHover:false
-      });   
-         
-      
+        pauseOnHover: false
+    });
+
+
     $('.gallery-text').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        pauseOnHover:false
-      });
-    
-      
+        pauseOnHover: false
+    });
+
+
     $('.gallery-text-2').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        pauseOnHover:false
-      });
-    
-      
+        pauseOnHover: false
+    });
+
+
 });
 // -------------------------------------------------------------------------------------------------------------------
 // password show
@@ -282,7 +282,7 @@ togglePassword.addEventListener("click", function () {
     // toggle the type attribute
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
-    
+
     // toggle the icon
     this.classList.toggle("fa-eye-slash");
 });
@@ -292,3 +292,28 @@ const form = document.querySelector("form");
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 });
+// ------------------------------------------------------------------------------------------------------------------------
+// cookies-box
+const cookieBox = document.querySelector(".cookies-main"),
+    buttons = document.querySelectorAll(".cookies-btn-1");
+
+const executeCodes = () => {
+    //if cookie contains codinglab it will be returned and below of this code will not run
+    if (document.cookie.includes("codinglab")) return;
+    cookieBox.classList.add("show");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            cookieBox.classList.remove("show");
+
+            //if button has acceptBtn id
+            if (button.id == "cookies-b-2") {
+                //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+                document.cookie = "cookieBy= codinglab; max-age=" + 60 * 60 * 24 * 30;
+            }
+        });
+    });
+};
+
+//executeCodes function will be called on webpage load
+window.addEventListener("load", executeCodes);
